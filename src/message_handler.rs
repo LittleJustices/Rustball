a2164a7cf486ac4afd35b641a101383a3a6ac6d4
JummyDeuce <justym2c@gmail.com>
2021-03-31 20:38:11 -0400

    separating out message handler

---
+++ src/message_handler.rs
@@ -0,0 +1,39 @@
+use std::fs;
+
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+pub struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+        if msg.content == "!pfp" {
+            let sauce = fs::read_to_string("PFP_Source.txt");
+
+            let answer = match sauce {
+                Ok(s) => format!("My profile picture is sourced from: {}", s),
+                Err(e) => {
+                    println!("Failed to read PFP source file: {:?}", e);
+                    "I'm sorry, I lost the source!".to_string()
+                }
+            };
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, answer).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }   
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
