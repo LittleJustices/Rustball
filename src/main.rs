5f4ecda25d62b6e128696a67f6f13b9f38400a9d
JummyDeuce <justym2c@gmail.com>
2021-04-15 00:06:10 -0400

    testing command framework

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-use std::fs;
+use std::{
+    fs,
+    collections::HashSet,
+};
 
 use serenity::{
+    framework::{
+        StandardFramework,
+        standard::{
+            CommandResult,
+            macros::{
+                command,
+                group,
+            },
+        },
+    },
+    http::Http,
+    model::channel::Message,
     prelude::*,
 };
 
@@ -9,12 +24,43 @@ use messaging::{
     message_handler::Handler,
 };
 
+#[group]
+#[commands(ping)]
+struct General;
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "Pong!").await?;
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
     let token = fs::read_to_string("DISCORD_TOKEN")
         .expect("Expected a token in the root folder");
 
+    let http = Http::new_with_token(&token);
+
+    let (owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owner.id);
+
+            (owners, info.id)
+        },
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    let framework = StandardFramework::new()
+        .configure(|c| c
+            .owners(owners)
+            .prefix("!")
+        )
+        .group(&GENERAL_GROUP);
+
     let mut client = Client::builder(&token)
+        .framework(framework)
         .event_handler(Handler::new())
         .await
         .expect("Error creating client");
