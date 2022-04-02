9841e2d83e4e359548961fe14a1501f68dac50fe
Little Justices <justym2c@gmail.com>
2022-04-02 20:38:43 +0200

    Add error types

--- src/math/math_errors.rs
+++ src/math/math_errors.rs
@@ -5,7 +5,9 @@ use std::{
 
 #[derive(Debug)]
 pub enum MathError {
-    PlaceholderError,
+    PlaceholderError,           // placeholder
+    ExpressionError(String),    // Malformed expression
+    SymbolError(String),        // Illegal symbols in expression
 }
 
 impl Error for MathError {}
@@ -13,7 +15,9 @@ impl Error for MathError {}
 impl fmt::Display for MathError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            MathError::PlaceholderError => write!(f, "Error handling TBA"),
+            MathError::PlaceholderError     => write!(f, "Error handling TBA"),
+            MathError::ExpressionError(why) => write!(f, "Something's wrong with that expression ! ∑(✘Д✘๑ ) {}", why),
+            MathError::SymbolError(why)     => write!(f, "`{}` Σ(・艸・○) What's this? I can't do math with that!", why)
         }
     }
 }
