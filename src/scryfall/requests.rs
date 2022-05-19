5596d72fe94447e47fc5ba8280327805d75dddbc
Little Justices <justym2c@gmail.com>
2022-05-19 19:57:42 +0200

    get text from scryfall

---
+++ src/scryfall/requests.rs
@@ -0,0 +1,20 @@
+use super::req_token::ReqToken;
+use reqwest;
+
+pub async fn get_scryfall_text(request_vector: Vec<ReqToken>) -> Result<String, reqwest::Error> {
+    let mut request_url = String::from("https://api.scryfall.com/cards/named?format=text&");
+    for token in request_vector.iter() {
+        match token {
+            ReqToken::Fuzzy(cardname) => {
+                request_url.push_str("fuzzy=");
+                request_url.push_str(&cardname);
+            },
+            _ => {
+                request_url.push_str("fuzzy=");
+                request_url.push_str("one with nothing");
+            }
+        }
+    }
+
+    reqwest::get(request_url).await?.text().await
+}
