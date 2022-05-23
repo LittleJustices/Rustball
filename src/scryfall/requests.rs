7e127612e806252566d5438d34737aeb84390269
Little Justices <justym2c@gmail.com>
2022-05-23 17:15:05 +0200

    get json functions

--- src/scryfall/requests.rs
+++ src/scryfall/requests.rs
@@ -1,4 +1,7 @@
-use super::req_token::ReqToken;
+use super::{
+    req_token::ReqToken,
+    card::Card,
+};
 use reqwest::Client;
 
 pub async fn get_scryfall_text(client: &Client, request_vector: Vec<ReqToken>) -> Result<String, reqwest::Error> {
@@ -19,8 +22,34 @@ pub async fn get_scryfall_text(client: &Client, request_vector: Vec<ReqToken>) -
     client.get(request_url).send().await?.text().await
 }
 
+#[allow(dead_code)]
+pub async fn get_scryfall_json(client: &Client, request_vector: Vec<ReqToken>) -> Result<Card, reqwest::Error> {
+    let mut request_url = String::from("https://api.scryfall.com/cards/named?");
+    for token in request_vector.iter() {
+        match token {
+            ReqToken::Fuzzy(cardname) => {
+                request_url.push_str("fuzzy=");
+                request_url.push_str(&cardname);
+            },
+            _ => {
+                request_url.push_str("fuzzy=");
+                request_url.push_str("one with nothing");
+            }
+        }
+    }
+
+    client.get(request_url).send().await?.json::<Card>().await
+}
+
 pub async fn get_scryfall_random_text(client: &Client) -> Result<String, reqwest::Error> {
     let request_url = "https://api.scryfall.com/cards/random?format=text";
 
     client.get(request_url).send().await?.text().await
+}
+
+#[allow(dead_code)]
+pub async fn get_scryfall_random_json(client: &Client) -> Result<Card, reqwest::Error> {
+    let request_url = "https://api.scryfall.com/cards/random";
+
+    client.get(request_url).send().await?.json::<Card>().await
 }
