997915adc15651860e325192e1570e3f6a7a4531
Little Justices <justym2c@gmail.com>
2022-06-03 21:03:04 +0200

    new dailydose entry

--- src/funsies/funsies.rs
+++ src/funsies/funsies.rs
@@ -39,7 +39,8 @@ pub fn dailydose() -> &'static str {
         "https://www.pixiv.net/artworks/1291597",
         "https://www.pixiv.net/artworks/56980398\nhttps://cdn.donmai.us/original/8e/fc/__hakurei_reimu_and_hieda_no_akyuu_touhou_drawn_by_kayako_tdxxxk__8efc14dafa57afd806c95f1ead3ffa8a.jpg",
         "https://www.pixiv.net/artworks/44898062",
-        "https://twitter.com/k0mamid0ri/status/815561664477073411"
+        "https://twitter.com/k0mamid0ri/status/815561664477073411",
+        "https://twitter.com/smalllightAON/status/1532765221240160261",
     ];
     let random_index = rand::thread_rng().gen_range(0..links.len());
     links[random_index]
