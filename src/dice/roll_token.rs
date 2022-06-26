59f012413d38e63162c3825824239c318747a7f7
Little Justices <justym2c@gmail.com>
2022-06-26 12:10:06 +0200

    convert between rol and rpn tokens

--- src/dice/roll_token.rs
+++ src/dice/roll_token.rs
@@ -1,4 +1,9 @@
-use crate::math::rpn_token::RpnToken;
+use std::convert::TryFrom;
+
+use crate::math::{
+    rpn_token::RpnToken, 
+    math_errors::MathError
+};
 use super::die::Die;
 
 #[allow(dead_code)]
@@ -11,6 +16,24 @@ pub enum RollToken {
     Reroll(Reroll),
     Target(Target),
     Botch(Target),
+}
+
+impl From<RpnToken> for RollToken {
+    fn from(rpn_token: RpnToken) -> Self {
+        RollToken::Math(rpn_token)
+    }
+}
+
+impl TryFrom<RollToken> for RpnToken {
+    type Error = MathError;
+
+    fn try_from(value: RollToken) -> Result<Self, Self::Error> {
+        match value {
+            RollToken::Math(rpn_token) => Ok(rpn_token),
+            RollToken::Die(die) => Ok(RpnToken::Number(die.result.into())),
+            _ => Err(MathError::PlaceholderError)
+        }
+    }
 }
 
 #[allow(dead_code)]
