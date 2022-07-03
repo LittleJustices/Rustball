6b42bb6c42d0faf0ed23bd75b753bfc2cf749386
Little Justices <justym2c@gmail.com>
2022-07-03 19:29:33 +0200

    typo fix

--- src/math/math_errors.rs
+++ src/math/math_errors.rs
@@ -17,7 +17,7 @@ impl fmt::Display for MathError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             MathError::PlaceholderError     => write!(f, "Error handling TBA"),
-            MathError::TokenError(why) => write!(f, "Some symbols must've ogtten mixed up! Can you check your math? {}", why),
+            MathError::TokenError(why) => write!(f, "Some symbols must've gotten mixed up! Can you check your math? {}", why),
             MathError::ExpressionError(why) => write!(f, "Something's wrong with that expression ! ∑(✘Д✘๑ ) {}", why),
             MathError::SymbolError(why)     => write!(f, "`{}` Σ(・艸・○) What's this? I can't do math with that!", why)
         }
