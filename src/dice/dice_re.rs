5ba63cb4581ea185522a2cba37140bd7a9b483d1
Little Justices <justym2c@gmail.com>
2022-07-16 17:00:21 +0200

    Dice token regex

--- src/dice/dice_re.rs
+++ src/dice/dice_re.rs
@@ -2,7 +2,26 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 const DICE_MATCH_STRING: &str = r"(?P<number>\d+)d(?P<sides>\d+)\s*(?:k?(?P<keep>[l|h]?)(?P<keepamt>\d*))";
+const DICE_TOKEN_STRING: &str = r"(?x)
+    (
+        [\^%\*x\/\+\-\(\)]  # Math operators
+        |
+        \d+\.?\d*           # Numbers
+        |
+        d                   # Dice notation
+        |
+        [tb]                # Target number or botch number
+        |
+        k[lh]?              # Keep
+        |
+        r[bwr]?             # Reroll
+        |
+        e[ar]?              # Explode
+        |
+        \[.*?\]             # Array
+    )";
 
 lazy_static!{
     pub static ref DICE_MATCH_RE: Regex = Regex::new(DICE_MATCH_STRING).expect("Failed to compile dice expression regex!");
+    pub static ref DICE_TOKEN_RE: Regex = Regex::new(DICE_TOKEN_STRING).expect("Failed to compile dice token regex!");
 }
