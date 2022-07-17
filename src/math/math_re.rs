5df3901d834c0ecba211421c052f77ba03fc1823
Little Justices <justym2c@gmail.com>
2022-07-17 13:04:25 +0200

    math regex

---
+++ src/math/math_re.rs
@@ -0,0 +1,8 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+const MATH_TOKEN_STRING: &str = r"([\^%\*x\/\+\-\(\)]|\d+\.?\d*)";
+
+lazy_static!{
+    pub static ref MATH_TOKEN_RE: Regex = Regex::new(MATH_TOKEN_STRING).expect("Failed to compile math token regex!");
+}
