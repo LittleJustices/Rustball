59ed59bf6020f2397fdf1aee0f4e85d971d4d1ba
Little Justices <justym2c@gmail.com>
2022-09-04 08:29:53 +0200

    new dailydose

--- src/funsies/funsies.rs
+++ src/funsies/funsies.rs
@@ -43,6 +43,7 @@ pub fn dailydose() -> &'static str {
         "https://twitter.com/smalllightAON/status/1532765221240160261",
         "https://twitter.com/Vtcsku3HJBR2eZw/status/1562432126091268097",
         "https://twitter.com/Vtcsku3HJBR2eZw/status/1561344299702165504",
+        "https://twitter.com/Vtcsku3HJBR2eZw/status/1566211831143604225"
     ];
     let random_index = rand::thread_rng().gen_range(0..links.len());
     links[random_index]
