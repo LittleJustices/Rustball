7a2982524092f7067537fdcd795733bb6480e9d4
Little Justices <justym2c@gmail.com>
2022-09-08 06:54:03 +0200

    separate out regex strings

--- src/dice/dice_re.rs
+++ src/dice/dice_re.rs
@@ -1,41 +1,23 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use crate::math::math_re::MATH_TOKEN_STRING;
 
 const DICE_MATCH_STRING: &str = r"(?P<number>\d+)d(?P<sides>\d+)";
-const DICE_TOKEN_STRING: &str = r"(?x)
-    (?P<token>
-        [\^%\*x/\+\-\(\)]   # Math operators
-        |
-        \d+\.?\d*           # Numbers
-        |
-        pi|π                # Pi
-        |
-        a?(?:sin|cos|tan)h? # Trig functions
-        |
-        sqrt|√              # Square root
-        |
-        abs                 # Absolute value
-        |
-        round               # rounding
-        |
-        rddown|rounddown|floor
-        |
-        rdup|roundup|ceil
-        |
-        d                   # Dice notation
-        |
-        [tb]                # Target number or botch number
-        |
-        k[elh]?             # Keep
-        |
-        r[obwr]?            # Reroll
-        |
-        e[aor]?             # Explode
-        |
-        \[.*?\]             # Array
-    )";
+const DICE_TOKEN_STRING: &str = r"
+    d                   # Dice notation
+    |
+    [tb]                # Target number or botch number
+    |
+    k[elh]?             # Keep
+    |
+    r[obwr]?            # Reroll
+    |
+    e[aor]?             # Explode
+    |
+    \[.*?\]             # Array
+";
 
 lazy_static!{
     pub static ref DICE_MATCH_RE: Regex = Regex::new(DICE_MATCH_STRING).expect("Failed to compile dice expression regex!");
-    pub static ref DICE_TOKEN_RE: Regex = Regex::new(DICE_TOKEN_STRING).expect("Failed to compile dice token regex!");
+    pub static ref DICE_TOKEN_RE: Regex = Regex::new(&format!("(?x)(?P<token>{}|{})", MATH_TOKEN_STRING, DICE_TOKEN_STRING)).expect("Failed to compile dice token regex!");
 }
--- src/math/math_re.rs
+++ src/math/math_re.rs
@@ -1,27 +1,26 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 
-const MATH_TOKEN_STRING: &str = r"(?x)
-    (?P<token>
-        [\^%\*x/\+\-\(\)]   # Math operators
-        |
-        \d+\.?\d*           # Numbers
-        |
-        pi|π                # Pi
-        |
-        a?(?:sin|cos|tan)h? # Trig functions
-        |
-        sqrt|√              # Square root
-        |
-        abs                 # Absolute value
-        |
-        round               # rounding
-        |
-        rddown|rounddown|floor
-        |
-        rdup|roundup|ceil
-    )";
+pub const MATH_TOKEN_STRING: &str = r"
+    [\^%\*x/\+\-\(\)]   # Math operators
+    |
+    \d+\.?\d*           # Numbers
+    |
+    pi|π                # Pi
+    |
+    a?(?:sin|cos|tan)h? # Trig functions
+    |
+    sqrt|√              # Square root
+    |
+    abs                 # Absolute value
+    |
+    round               # rounding
+    |
+    rddown|rounddown|floor
+    |
+    rdup|roundup|ceil
+";
 
 lazy_static!{
-    pub static ref MATH_TOKEN_RE: Regex = Regex::new(MATH_TOKEN_STRING).expect("Failed to compile math token regex!");
+    pub static ref MATH_TOKEN_RE: Regex = Regex::new(&format!("(?x)(?P<token>{})", MATH_TOKEN_STRING)).expect("Failed to compile math token regex!");
 }
