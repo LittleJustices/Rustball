96a435e0a7ed0472b0f4680d66314d0bc528a600
Little Justices <justym2c@gmail.com>
2022-09-29 10:29:24 +0200

    genesys regex

--- src/dice/dice_re.rs
+++ src/dice/dice_re.rs
@@ -16,6 +16,13 @@ const DICE_TOKEN_STRING: &str = r"
     \[.*?\]             # Array
 ";
 
+const GENESYS_TOKEN_STRING: &str = r"(?x)
+    (?P<type>[bsadpc])  # Types of dice, named capture group
+    \s*                 # Allow any number of spaces
+    (?P<number>\d+)     # Number of dice of the preceding kind
+";
+
 lazy_static!{
     pub static ref DICE_TOKEN_RE: Regex = Regex::new(&format!("(?x)(?P<token>{}|{})", MATH_TOKEN_STRING, DICE_TOKEN_STRING)).expect("Failed to compile dice token regex!");
+    pub static ref GENESYS_TOKEN_RE: Regex = Regex::new(GENESYS_TOKEN_STRING).expect("Failed to compile genesys token regex!");
 }
