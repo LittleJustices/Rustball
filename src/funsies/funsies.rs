be34312f9691a8da1b7e05824e397ba90c533b18
Little Justices <justym2c@gmail.com>
2022-10-14 09:02:40 +0200

    remove broken link

--- src/funsies/funsies.rs
+++ src/funsies/funsies.rs
@@ -37,7 +37,6 @@ pub fn dailydose() -> &'static str {
         "https://www.pixiv.net/artworks/92405212",
         "https://www.pixiv.net/artworks/65369107",
         "https://www.pixiv.net/artworks/52084017",
-        "https://www.pixiv.net/artworks/13378064",
         "https://www.pixiv.net/artworks/1291597",
         "https://www.pixiv.net/artworks/56980398\nhttps://cdn.donmai.us/original/8e/fc/__hakurei_reimu_and_hieda_no_akyuu_touhou_drawn_by_kayako_tdxxxk__8efc14dafa57afd806c95f1ead3ffa8a.jpg",
         "https://www.pixiv.net/artworks/44898062",
