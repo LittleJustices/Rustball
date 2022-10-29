6715c8d5a0317ba5a8e9bb69091f63ab3e2fefed
Little Justices <justym2c@gmail.com>
2022-10-29 12:36:52 +0200

    display for roll value

--- src/dice/roll_value.rs
+++ src/dice/roll_value.rs
@@ -1,7 +1,9 @@
 use super::{
     dice_errors::RollError,
 };
+use std::fmt;
 
+#[derive(Debug)]
 pub enum RollValue {
     Decimal(f64),
     Successes(i16),
@@ -25,6 +27,15 @@ impl RollValue {
                 RollValue::Decimal(right) => Ok(RollValue::Decimal((left as f64) + right)),
                 RollValue::Successes(right) => Ok(RollValue::Successes(left + right)),
             },
+        }
+    }
+}
+
+impl fmt::Display for RollValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RollValue::Decimal(number) => write!(f, "{}", number),
+            RollValue::Successes(sux) => write!(f, "{}", sux),
         }
     }
 }
