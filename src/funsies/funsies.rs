3e00860823ee84e6d9772274eee8edeff4262d09
Little Justices <justym2c@gmail.com>
2023-02-18 19:53:48 +0100

    new dailydose

--- src/funsies/funsies.rs
+++ src/funsies/funsies.rs
@@ -32,6 +32,7 @@ pub fn dailydose() -> &'static str {
             "https://twitter.com/kyomoneko_2/status/1485186556210163713",
             "https://twitter.com/bon_feather/status/1567113632814620676",
             "https://twitter.com/bon_feather/status/1620748694147706880",
+            "https://twitter.com/bon_feather/status/1626925749629321217",
             "https://skeb.jp/@Hisona_/works/278\nhttps://media.discordapp.net/attachments/245782979710812161/1044659249009135616/951808-1.output.png",
             "https://twitter.com/kyomoneko_2/status/1595693938798567425",
         ],
