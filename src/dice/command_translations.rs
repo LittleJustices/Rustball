ff6d1d2e8522c528877acc3c672e51cafea48f86
Little Justices <justym2c@gmail.com>
2023-03-23 15:31:08 +0100

    add math to s3roll

--- src/dice/command_translations.rs
+++ src/dice/command_translations.rs
@@ -22,12 +22,23 @@ pub fn genesys(in_command: &str) -> Result<String, RollError> {
     Ok(out_command)
 }
 
-pub fn story_shaper(_in_command: &str) -> Result<String, RollError> {
-    Ok(format!("2d10"))
+pub fn story_shaper(in_command: &str) -> Result<String, RollError> {
+    let mut out_command = String::from("2d10");
+    for caps in S3_TOKEN_RE.captures_iter(in_command) {
+        let modifier = match caps.name("mod") {
+            Some(m) => m.as_str(),
+            None => "",
+        };
+        out_command.push_str(modifier);
+    }
+
+    Ok(out_command)
 }
 
 #[cfg(test)]
 mod tests {
+    use std::vec;
+
     use super::*;
 
     #[test]
@@ -36,5 +47,15 @@ mod tests {
         let out_command = genesys(in_command).unwrap();
 
         assert_eq!(out_command, "2d8ga&2d12gp&2d6gb&2d8gd&2d12gc&2d6gs".to_owned());
+    }
+
+    #[test]
+    fn test_3s() {
+        let in_commands: Vec<&str> = vec!["", "+5", "-(2+3)*4/5"];
+        let out_commands: Vec<&str> = vec!["2d10", "2d10+5", "2d10-(2+3)*4/5"];
+
+        for i in 0..in_commands.len() {
+            assert_eq!(out_commands[i], story_shaper(in_commands[i]).unwrap());
+        }
     }
 }
