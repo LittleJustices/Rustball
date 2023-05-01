a70987146170e3888f41b57199e0b0343b3cd5d2
Little Justices <justym2c@gmail.com>
2023-05-01 20:36:42 +0200

    new dailydose

--- src/funsies/funsies.rs
+++ src/funsies/funsies.rs
@@ -62,6 +62,7 @@ pub fn dailydose() -> &'static str {
             "https://twitter.com/PileArcato/status/1641288827368099841",
             "https://twitter.com/PileArcato/status/1641120552373960707",
             "https://twitter.com/Kuzukago_123/status/1646491966434349056",
+            "https://twitter.com/31mriri0830/status/1652673110141976576",
         ]
     };
     let random_index = rand::thread_rng().gen_range(0..links.len());
